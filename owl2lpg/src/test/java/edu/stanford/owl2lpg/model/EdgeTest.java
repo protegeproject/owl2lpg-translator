ee34229ebb72c0d218400c1369e6f09d4af520c3
johardi <josef.hardi@stanford.edu>
2020-04-03 09:52:07 -0700

    test: added a unit test for Node and Edge

--- owl2lpg/src/test/java/edu/stanford/owl2lpg/model/EdgeTest.java
+++ owl2lpg/src/test/java/edu/stanford/owl2lpg/model/EdgeTest.java
@@ -1,6 +1,76 @@
 package edu.stanford.owl2lpg.model;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class EdgeTest {
 
+  private Edge edge;
 
+  @Mock
+  private Node fromNode, toNode;
+
+  @Mock
+  private Properties properties;
+
+  @Before
+  public void setUp() {
+    edge = Edge.create(fromNode, toNode, "anEdge", properties);
+  }
+
+  @Test
+  public void shouldCreateEdge() {
+    assertThat(edge, is(notNullValue()));
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void shouldThrowNPEWhenFromNodeNull() {
+    Edge.create(null, toNode, "anEdge", properties);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void shouldThrowNPEWhenToNodeNull() {
+    Edge.create(fromNode, null, "anEdge", properties);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void shouldThrowNPEWhenLabelNull() {
+    Edge.create(fromNode, toNode, null, properties);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void shouldThrowNPEWhenPropertiesNull() {
+    Edge.create(fromNode, toNode, "anEdge", null);
+  }
+
+  @Test
+  public void shouldGetFromNode() {
+    var actualNode = edge.getFromNode();
+    assertThat(actualNode, equalTo(fromNode));
+  }
+
+  @Test
+  public void shouldGetToNode() {
+    var actualNode = edge.getToNode();
+    assertThat(actualNode, equalTo(toNode));
+  }
+
+  @Test
+  public void shouldGetLabel() {
+    var actualLabel = edge.getLabel();
+    assertThat(actualLabel, equalTo("anEdge"));
+  }
+
+  @Test
+  public void shouldGetProperties() {
+    var actualProperties = edge.getProperties();
+    assertThat(actualProperties, equalTo(properties));
+  }
 }
---
+++ owl2lpg/src/test/java/edu/stanford/owl2lpg/model/NodeTest.java
@@ -0,0 +1,55 @@
+package edu.stanford.owl2lpg.model;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NodeTest {
+
+  private Node node;
+
+  @Mock
+  private ImmutableList<String> labels;
+
+  @Mock
+  private Properties properties;
+
+  @Before
+  public void setUp() {
+    node = Node.create(labels, properties);
+  }
+
+  @Test
+  public void shouldCreateNode() {
+    assertThat(node, is(notNullValue()));
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void shouldThrowNPEWhenLabelsNull() {
+    Node.create(null, properties);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void shouldThrowNPEWhenPropertiesNull() {
+    Node.create(labels, null);
+  }
+
+  @Test
+  public void getLabels() {
+    var actualLabels = node.getLabels();
+    assertThat(actualLabels, equalTo(labels));
+  }
+
+  @Test
+  public void getProperties() {
+    var actualProperties = node.getProperties();
+    assertThat(actualProperties, equalTo(properties));
+  }
+}
