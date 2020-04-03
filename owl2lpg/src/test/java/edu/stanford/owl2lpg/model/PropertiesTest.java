5155e96e6a99ec2b319415666d1297073befc402
johardi <josef.hardi@stanford.edu>
2020-04-03 09:51:20 -0700

    test: added more unit testing

--- owl2lpg/src/test/java/edu/stanford/owl2lpg/model/PropertiesTest.java
+++ owl2lpg/src/test/java/edu/stanford/owl2lpg/model/PropertiesTest.java
@@ -3,31 +3,62 @@ package edu.stanford.owl2lpg.model;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PropertiesTest {
 
   private Properties properties;
 
+  @Mock
+  private ImmutableMap<String, Object> map;
+
   @Before
   public void setUp() {
+    when(map.get("key")).thenReturn("value");
+  }
 
+  @Test
+  public void shouldCreateEmptyProperties() {
+    properties = Properties.empty();
+    assertThat(properties, is(notNullValue()));
+  }
+
+  @Test
+  public void shouldCreateProperties() {
+    properties = Properties.create(map);
+    assertThat(properties, is(notNullValue()));
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void shouldThrowNPEWhenMapNull() {
+    properties.create(null);
   }
 
   @Test
   public void shouldGetEmptyMap() {
     properties = Properties.empty();
-    var map = properties.getMap();
-    assertThat(map, equalTo(ImmutableMap.of()));
+    var actualMap = properties.getMap();
+    assertThat(actualMap, equalTo(ImmutableMap.of()));
   }
 
   @Test
   public void shouldGetSuppliedMap() {
-    var suppliedMap = ImmutableMap.<String, Object>of("key", "value");
-    properties = Properties.create(suppliedMap);
-    var map = properties.getMap();
-    assertThat(map, equalTo(suppliedMap));
+    properties = Properties.create(map);
+    var actualMap = properties.getMap();
+    assertThat(actualMap, equalTo(map));
+  }
+
+  @Test
+  public void shouldGetValueFromKey() {
+    properties = Properties.create(map);
+    var actualValue = properties.get("key");
+    assertThat(actualValue, equalTo("value"));
   }
 }
