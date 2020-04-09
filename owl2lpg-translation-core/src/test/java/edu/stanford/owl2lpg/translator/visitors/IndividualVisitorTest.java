5887e2746ecba188e90495421f0bbacf5a84db30
johardi <josef.hardi@stanford.edu>
2020-04-09 11:44:26 -0700

    test: added UTs for the IndividualVisitor

---
+++ owl2lpg-translation-core/src/test/java/edu/stanford/owl2lpg/translator/visitors/IndividualVisitorTest.java
@@ -0,0 +1,48 @@
+package edu.stanford.owl2lpg.translator.visitors;
+
+import edu.stanford.owl2lpg.translator.vocab.NodeLabels;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.semanticweb.owlapi.model.OWLAnonymousIndividual;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IndividualVisitorTest {
+
+  @Mock
+  private EntityVisitor entityVisitor;
+
+  private IndividualVisitor visitor;
+
+  @Before
+  public void setUp() {
+    visitor = spy(new IndividualVisitor(entityVisitor));
+  }
+
+  @Test
+  public void shouldVisitNamedIndividual() {
+    var individual = mock(OWLNamedIndividual.class);
+    visitor.visit(individual);
+    verify(visitor).visit(individual);
+    verify(entityVisitor).visit(individual);
+  }
+
+  @Test
+  public void shouldVisitAnonymousIndividual() {
+    var individual = mock(OWLAnonymousIndividual.class);
+    visitor.visit(individual);
+    verify(visitor).visit(individual);
+    verify(visitor).createMainNode(individual, NodeLabels.ANONYMOUS_INDIVIDUAL);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void shouldThrowNPEWhenIndividualNull() {
+    OWLAnonymousIndividual nullIndividual = null;
+    visitor.visit(nullIndividual);
+  }
+}
