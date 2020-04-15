2c94d6c803c75c6308d2cc2f8eedb3cedaca3d9f
johardi <josef.hardi@stanford.edu>
2020-04-14 23:50:14 -0700

    test: added a test suite for all visitor tests

---
+++ owl2lpg-translation-core/src/test/java/edu/stanford/owl2lpg/translator/visitors/VisitorTestSuite.java
@@ -0,0 +1,19 @@
+package edu.stanford.owl2lpg.translator.visitors;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    EntityVisitorTest.class,
+    ClassExpressionVisitorTest.class,
+    PropertyExpressionVisitorTest.class,
+    IndividualVisitorTest.class,
+    DataVisitorTest.class,
+    AnnotationSubjectVisitorTest.class,
+    AnnotationValueVisitorTest.class,
+    AxiomVisitorTest.class
+})
+public class VisitorTestSuite {
+  // NO-OP
+}
