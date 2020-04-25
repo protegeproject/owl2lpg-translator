ea5b667ee7cf2c36f68f5365247a02b0954e374d
johardi <josef.hardi@stanford.edu>
2020-04-24 18:09:49 -0700

    feat: added java options in the run script

--- owl2lpg-translation-cli/src/main/scripts/run.sh
+++ owl2lpg-translation-cli/src/main/scripts/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 java=java
+java_opts="-Xms8g -Xmx16g"
 if test -n "$JAVA_HOME"; then
   java="$JAVA_HOME/bin/java"
 fi
-exec "$java" $java_args -jar owl2lpg-translation-cli.jar "$@"
+exec "$java" $java_opts -jar owl2lpg-translation-cli.jar "$@"
 exit 1
