9d3425055c571601ac6c88dbad794e4091917bb0
johardi <josef.hardi@stanford.edu>
2020-05-11 09:39:00 -0700

    fix: renamed class

---
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/frame/ObjectPropertyAccessor.java
@@ -0,0 +1,8 @@
+package edu.stanford.owl2lpg.client.read.frame;
+
+/**
+ * @author Josef Hardi <josef.hardi@stanford.edu> <br>
+ * Stanford Center for Biomedical Informatics Research
+ */
+public class ObjectPropertyAccessor {
+}
--- owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/frame/ObjectPropertyFrameAccessorImpl.java
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/frame/ObjectPropertyFrameAccessorImpl.java
@@ -4,5 +4,5 @@ package edu.stanford.owl2lpg.client.read.frame;
  * @author Josef Hardi <josef.hardi@stanford.edu> <br>
  * Stanford Center for Biomedical Informatics Research
  */
-public class ObjectPropertyFrameAccessor {
+public class ObjectPropertyFrameAccessorImpl {
 }
