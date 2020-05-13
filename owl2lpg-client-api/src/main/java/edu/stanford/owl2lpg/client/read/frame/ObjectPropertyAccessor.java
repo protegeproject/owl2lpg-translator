cddb6f3e3bda61f5aaf6fc061423f2a8f6c35f80
johardi <josef.hardi@stanford.edu>
2020-05-13 09:12:33 -0700

    feat: skeleton for the object property accessor

--- owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/frame/ObjectPropertyAccessor.java
+++
@@ -1,8 +0,0 @@
-package edu.stanford.owl2lpg.client.read.frame;
-
-/**
- * @author Josef Hardi <josef.hardi@stanford.edu> <br>
- * Stanford Center for Biomedical Informatics Research
- */
-public class ObjectPropertyAccessor {
-}
--- owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/frame/ObjectPropertyFrameAccessorImpl.java
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/frame/ObjectPropertyFrameAccessorImpl.java
@@ -1,9 +1,44 @@
 package edu.stanford.owl2lpg.client.read.frame;
 
+import edu.stanford.bmir.protege.web.shared.frame.ObjectPropertyFrame;
+import edu.stanford.owl2lpg.versioning.model.AxiomContext;
+import org.neo4j.driver.Session;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+
+import javax.annotation.Nonnull;
+import javax.inject.Provider;
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * @author Josef Hardi <josef.hardi@stanford.edu> <br>
  * Stanford Center for Biomedical Informatics Research
  */
 public class ObjectPropertyFrameAccessorImpl
     implements ObjectPropertyFrameAccessor, AutoCloseable {
+
+  @Nonnull
+  private final Session session;
+
+  @Nonnull
+  private final Provider<ObjectPropertyFrameRecordHandler> provider;
+
+  public ObjectPropertyFrameAccessorImpl(@Nonnull Session session,
+                                         @Nonnull Provider<ObjectPropertyFrameRecordHandler> provider) {
+    this.session = checkNotNull(session);
+    this.provider = checkNotNull(provider);
+  }
+
+  @Nonnull
+  @Override
+  public Optional<ObjectPropertyFrame> getFrame(@Nonnull AxiomContext context,
+                                                @Nonnull OWLObjectProperty subject) {
+    return Optional.empty();
+  }
+
+  @Override
+  public void close() throws Exception {
+    session.close();
+  }
 }
