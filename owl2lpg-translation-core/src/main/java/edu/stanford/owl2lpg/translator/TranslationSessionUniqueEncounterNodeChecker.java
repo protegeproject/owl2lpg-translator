a22b74fef15d1d19a4a2cd29d4f003f80187597e
Matthew Horridge <matthew.horridge@stanford.edu>
2020-06-02 10:21:03 -0700

    More explicity documentation

--- owl2lpg-translation-core/src/main/java/edu/stanford/owl2lpg/translator/TranslationSessionUniqueEncounterNodeChecker.java
+++ owl2lpg-translation-core/src/main/java/edu/stanford/owl2lpg/translator/TranslationSessionUniqueEncounterNodeChecker.java
@@ -3,7 +3,7 @@ package edu.stanford.owl2lpg.translator;
 public interface TranslationSessionUniqueEncounterNodeChecker {
 
     /**
-     * Determines whether the specified node will only be encountered once
+     * Determines whether the specified node object will only be encountered once
      * in a translation session
      * @param o The node object.
      * @return true if the object will not be encountered more than once in a translation
