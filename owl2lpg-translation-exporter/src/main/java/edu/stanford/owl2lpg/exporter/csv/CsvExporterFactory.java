d33b1678fc3342c3b2cb1f3ac6bdac0f61c1d0fd
Josef Hardi <josef.hardi@gmail.com>
2020-06-16 11:43:22 -0700

    feat: implemented an export tracker

--- owl2lpg-translation-exporter/src/main/java/edu/stanford/owl2lpg/exporter/csv/CsvExporterFactory.java
+++ owl2lpg-translation-exporter/src/main/java/edu/stanford/owl2lpg/exporter/csv/CsvExporterFactory.java
@@ -11,35 +11,41 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 public class CsvExporterFactory {
 
-    @Nonnull
-    private final OntologyDocumentAxiomTranslator ontologyDocumentAxiomTranslator;
+  @Nonnull
+  private final OntologyDocumentAxiomTranslator ontologyDocumentAxiomTranslator;
 
-    @Nonnull
-    private final NodesCsvWriterFactory nodesCsvWriterFactory;
+  @Nonnull
+  private final NodesCsvWriterFactory nodesCsvWriterFactory;
 
-    @Nonnull
-    private final RelationshipsCsvWriterFactory relationshipsCsvWriterFactory;
+  @Nonnull
+  private final RelationshipsCsvWriterFactory relationshipsCsvWriterFactory;
 
-    @Nonnull
-    private final TranslationSessionNodeObjectSingleEncounterChecker nodeEncounterChecker;
+  @Nonnull
+  private final TranslationSessionNodeObjectSingleEncounterChecker nodeEncounterChecker;
 
-    @Inject
-    public CsvExporterFactory(@Nonnull OntologyDocumentAxiomTranslator ontologyDocumentAxiomTranslator,
-                              @Nonnull NodesCsvWriterFactory nodesCsvWriterFactory,
-                              @Nonnull RelationshipsCsvWriterFactory relationshipsCsvWriterFactory,
-                              @Nonnull TranslationSessionNodeObjectSingleEncounterChecker nodeEncounterChecker) {
-        this.ontologyDocumentAxiomTranslator = checkNotNull(ontologyDocumentAxiomTranslator);
-        this.nodesCsvWriterFactory = checkNotNull(nodesCsvWriterFactory);
-        this.relationshipsCsvWriterFactory = checkNotNull(relationshipsCsvWriterFactory);
-        this.nodeEncounterChecker = checkNotNull(nodeEncounterChecker);
-    }
+  @Nonnull
+  private final CsvExportChecker csvExportChecker;
 
-    @Nonnull
-    public CsvExporter create(@Nonnull Writer nodesCsvWriter,
-                              @Nonnull Writer relationshipsCsvWriter) {
-        return new CsvExporter(ontologyDocumentAxiomTranslator,
-            nodesCsvWriterFactory.create(nodesCsvWriter),
-            relationshipsCsvWriterFactory.create(relationshipsCsvWriter),
-            nodeEncounterChecker);
-    }
+  @Inject
+  public CsvExporterFactory(@Nonnull OntologyDocumentAxiomTranslator ontologyDocumentAxiomTranslator,
+                            @Nonnull NodesCsvWriterFactory nodesCsvWriterFactory,
+                            @Nonnull RelationshipsCsvWriterFactory relationshipsCsvWriterFactory,
+                            @Nonnull TranslationSessionNodeObjectSingleEncounterChecker nodeEncounterChecker,
+                            @Nonnull CsvExportChecker csvExportChecker) {
+    this.ontologyDocumentAxiomTranslator = checkNotNull(ontologyDocumentAxiomTranslator);
+    this.nodesCsvWriterFactory = checkNotNull(nodesCsvWriterFactory);
+    this.relationshipsCsvWriterFactory = checkNotNull(relationshipsCsvWriterFactory);
+    this.nodeEncounterChecker = checkNotNull(nodeEncounterChecker);
+    this.csvExportChecker = checkNotNull(csvExportChecker);
+  }
+
+  @Nonnull
+  public CsvExporter create(@Nonnull Writer nodesCsvWriter,
+                            @Nonnull Writer relationshipsCsvWriter) {
+    return new CsvExporter(ontologyDocumentAxiomTranslator,
+        nodesCsvWriterFactory.create(nodesCsvWriter),
+        relationshipsCsvWriterFactory.create(relationshipsCsvWriter),
+        nodeEncounterChecker,
+        csvExportChecker);
+  }
 }
