db9a720903fcedc00c1617cd34835b86113f32e0
Josef Hardi <josef.hardi@gmail.com>
2020-06-16 18:19:28 -0700

    fix: A single translation session will encounter an annotation once

--- owl2lpg-translation-core/src/main/java/edu/stanford/owl2lpg/translator/SingleEncounterNodeCheckerImpl.java
+++ owl2lpg-translation-core/src/main/java/edu/stanford/owl2lpg/translator/SingleEncounterNodeCheckerImpl.java
@@ -1,5 +1,6 @@
 package edu.stanford.owl2lpg.translator;
 
+import org.semanticweb.owlapi.model.OWLAnnotation;
 import org.semanticweb.owlapi.model.OWLAxiom;
 
 import javax.inject.Inject;
@@ -12,6 +13,7 @@ public class SingleEncounterNodeCheckerImpl implements SingleEncounterNodeChecke
 
   @Override
   public boolean isSingleEncounterNodeObject(Object o) {
-    return o instanceof OWLAxiom;
+    return o instanceof OWLAxiom
+        || o instanceof OWLAnnotation;
   }
 }
