4b7beb008647b67d86430647bb737b2f76a0b327
Josef Hardi <josef.hardi@gmail.com>
2020-07-07 11:22:45 -0700

    fix: removed deprecated classes

--- owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/axiom/DataAccessorComponent.java
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/axiom/DataAccessorComponent.java
@@ -7,13 +7,9 @@ import edu.stanford.owl2lpg.client.DatabaseSessionScope;
  * @author Josef Hardi <josef.hardi@stanford.edu> <br>
  * Stanford Center for Biomedical Informatics Research
  */
-@Component(modules = {
-    FrameAxiomAccessorModule.class,
-    ShortFormAccessorModule.class})
+@Component(modules = FrameAxiomAccessorModule.class)
 @DatabaseSessionScope
 public interface DataAccessorComponent {
 
   FrameAxiomAccessor getFrameAxiomAccessor();
-
-  ShortFormAccessor getShortFormAccessor();
 }
