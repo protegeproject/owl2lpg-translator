795de1360d7aa10ad51b942719568cc732cfed3c
Josef Hardi <josef.hardi@gmail.com>
2020-07-07 13:55:36 -0700

    fix: updated the class dependencies

--- owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/frame2/CypherClassFrameAxiomsIndex.java
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/frame2/CypherClassFrameAxiomsIndex.java
@@ -2,21 +2,18 @@ package edu.stanford.owl2lpg.client.read.frame2;
 
 import com.google.common.collect.ImmutableSet;
 import edu.stanford.bmir.protege.web.server.index.ClassFrameAxiomsIndex;
-import edu.stanford.owl2lpg.client.read.axiom.FrameAxiomAccessor;
+import edu.stanford.owl2lpg.client.read.axiom.AxiomSubjectAccessor;
 import edu.stanford.owl2lpg.model.AxiomContext;
 import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static edu.stanford.bmir.protege.web.server.index.ClassFrameAxiomsIndex.AnnotationsTreatment.EXCLUDE_ANNOTATIONS;
-import static edu.stanford.bmir.protege.web.server.index.ClassFrameAxiomsIndex.AnnotationsTreatment.INCLUDE_ANNOTATIONS;
 
 /**
  * @author Josef Hardi <josef.hardi@stanford.edu> <br>
@@ -28,18 +25,18 @@ public class CypherClassFrameAxiomsIndex implements ClassFrameAxiomsIndex {
   private final AxiomContext axiomContext;
 
   @Nonnull
-  private final FrameAxiomAccessor frameAxiomAccessor;
+  private final AxiomSubjectAccessor axiomSubjectAccessor;
 
   @Inject
   public CypherClassFrameAxiomsIndex(@Nonnull AxiomContext axiomContext,
-                                     @Nonnull FrameAxiomAccessor frameAxiomAccessor) {
+                                     @Nonnull AxiomSubjectAccessor axiomSubjectAccessor) {
     this.axiomContext = checkNotNull(axiomContext);
-    this.frameAxiomAccessor = checkNotNull(frameAxiomAccessor);
+    this.axiomSubjectAccessor = checkNotNull(axiomSubjectAccessor);
   }
 
   @Override
   public Set<OWLAxiom> getFrameAxioms(OWLClass owlClass, AnnotationsTreatment annotationsTreatment) {
-    return frameAxiomAccessor.getFrameAxioms(axiomContext, owlClass)
+    return axiomSubjectAccessor.getAxiomSubject(axiomContext, owlClass)
         .stream()
         .filter(axiom -> {
           var accepted = true;
@@ -49,13 +46,5 @@ public class CypherClassFrameAxiomsIndex implements ClassFrameAxiomsIndex {
           return accepted;
         })
         .collect(ImmutableSet.toImmutableSet());
-  }
-
-  @Override
-  public Stream<OWLSubClassOfAxiom> getFrameSubClassOfAxioms(OWLClass owlClass) {
-    return getFrameAxioms(owlClass, INCLUDE_ANNOTATIONS)
-        .stream()
-        .filter(OWLSubClassOfAxiom.class::isInstance)
-        .map(OWLSubClassOfAxiom.class::cast);
   }
 }
