7100f9de5d4760232d03a5372781ada048d9a0a1
Josef Hardi <josef.hardi@gmail.com>
2020-08-05 09:24:00 -0700

    fix(translation-core): extract project context creation methods to create a factory

--- owl2lpg-translation-core/src/main/java/edu/stanford/owl2lpg/translator/visitors/OntologyProjectVisitor.java
+++ owl2lpg-translation-core/src/main/java/edu/stanford/owl2lpg/translator/visitors/OntologyProjectVisitor.java
@@ -7,10 +7,8 @@ import edu.stanford.owl2lpg.model.BranchId;
 import edu.stanford.owl2lpg.model.Edge;
 import edu.stanford.owl2lpg.model.EdgeFactory;
 import edu.stanford.owl2lpg.model.Node;
-import edu.stanford.owl2lpg.model.NodeFactory;
 import edu.stanford.owl2lpg.model.OntologyDocumentId;
 import edu.stanford.owl2lpg.model.ProjectId;
-import edu.stanford.owl2lpg.model.Properties;
 import edu.stanford.owl2lpg.translator.AnnotationObjectTranslator;
 import edu.stanford.owl2lpg.translator.AnnotationValueTranslator;
 import edu.stanford.owl2lpg.translator.AxiomTranslator;
@@ -38,12 +36,6 @@ import static edu.stanford.owl2lpg.translator.vocab.EdgeLabel.AXIOM;
 import static edu.stanford.owl2lpg.translator.vocab.EdgeLabel.ONTOLOGY_ANNOTATION;
 import static edu.stanford.owl2lpg.translator.vocab.EdgeLabel.ONTOLOGY_IRI;
 import static edu.stanford.owl2lpg.translator.vocab.EdgeLabel.VERSION_IRI;
-import static edu.stanford.owl2lpg.translator.vocab.NodeLabels.BRANCH;
-import static edu.stanford.owl2lpg.translator.vocab.NodeLabels.ONTOLOGY_DOCUMENT;
-import static edu.stanford.owl2lpg.translator.vocab.NodeLabels.PROJECT;
-import static edu.stanford.owl2lpg.translator.vocab.PropertyFields.BRANCH_ID;
-import static edu.stanford.owl2lpg.translator.vocab.PropertyFields.ONTOLOGY_DOCUMENT_ID;
-import static edu.stanford.owl2lpg.translator.vocab.PropertyFields.PROJECT_ID;
 
 /**
  * @author Josef Hardi <josef.hardi@stanford.edu> <br>
@@ -61,7 +53,7 @@ public class OntologyProjectVisitor implements OWLNamedObjectVisitorEx<Translati
   private final OntologyDocumentId ontoDocId;
 
   @Nonnull
-  private final NodeFactory nodeFactory;
+  private final ProjectContextNodeFactory projectContextNodeFactory;
 
   @Nonnull
   private final EdgeFactory edgeFactory;
@@ -82,29 +74,29 @@ public class OntologyProjectVisitor implements OWLNamedObjectVisitorEx<Translati
   public OntologyProjectVisitor(@Nonnull ProjectId projectId,
                                 @Nonnull BranchId branchId,
                                 @Nonnull OntologyDocumentId ontoDocId,
-                                @Nonnull NodeFactory nodeFactory,
+                                @Nonnull ProjectContextNodeFactory projectContextNodeFactory,
                                 @Nonnull EdgeFactory edgeFactory,
                                 @Nonnull EntityTranslator entityTranslator,
                                 @Nonnull AnnotationValueTranslator annotationValueTranslator,
                                 @Nonnull AnnotationObjectTranslator annotationObjectTranslator,
                                 @Nonnull AxiomTranslator axiomTranslator) {
     this.projectId = checkNotNull(projectId);
-    this.branchId = branchId;
-    this.ontoDocId = ontoDocId;
-    this.nodeFactory = nodeFactory;
-    this.edgeFactory = edgeFactory;
-    this.entityTranslator = entityTranslator;
-    this.annotationValueTranslator = annotationValueTranslator;
-    this.annotationObjectTranslator = annotationObjectTranslator;
-    this.axiomTranslator = axiomTranslator;
+    this.branchId = checkNotNull(branchId);
+    this.ontoDocId = checkNotNull(ontoDocId);
+    this.projectContextNodeFactory = checkNotNull(projectContextNodeFactory);
+    this.edgeFactory = checkNotNull(edgeFactory);
+    this.entityTranslator = checkNotNull(entityTranslator);
+    this.annotationValueTranslator = checkNotNull(annotationValueTranslator);
+    this.annotationObjectTranslator = checkNotNull(annotationObjectTranslator);
+    this.axiomTranslator = checkNotNull(axiomTranslator);
   }
 
   @Nonnull
   @Override
   public Translation visit(OWLOntology ontology) {
-    var projectNode = createProjectNode(projectId);
-    var branchNode = createBranchNode(branchId);
-    var ontoDocNode = createOntologyDocumentNode(ontoDocId);
+    var projectNode = projectContextNodeFactory.createProjectNode(projectId);
+    var branchNode = projectContextNodeFactory.createBranchNode(branchId);
+    var ontoDocNode = projectContextNodeFactory.createOntologyDocumentNode(ontoDocId);
 
     var translations = new Builder<Translation>();
     var edges = new Builder<Edge>();
@@ -123,18 +115,6 @@ public class OntologyProjectVisitor implements OWLNamedObjectVisitorEx<Translati
                 ontoDocNode,
                 edges.build(),
                 translations.build())))));
-  }
-
-  private Node createProjectNode(@Nonnull ProjectId projectId) {
-    return nodeFactory.createNode(projectId, PROJECT, Properties.of(PROJECT_ID, projectId.getIdentifier()));
-  }
-
-  private Node createBranchNode(@Nonnull BranchId branchId) {
-    return nodeFactory.createNode(branchId, BRANCH, Properties.of(BRANCH_ID, branchId.getIdentifier()));
-  }
-
-  private Node createOntologyDocumentNode(@Nonnull OntologyDocumentId ontoDocId) {
-    return nodeFactory.createNode(ontoDocId, ONTOLOGY_DOCUMENT, Properties.of(ONTOLOGY_DOCUMENT_ID, ontoDocId.getIdentifier()));
   }
 
   private void translateOntologyIri(Optional<IRI> ontologyIri, Node ontoDocNode, Builder<Translation> translations, Builder<Edge> edges) {
