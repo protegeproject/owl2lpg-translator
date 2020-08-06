850c07a0d6e5d864cb7036afad2ab06e8cd4a256
Josef Hardi <josef.hardi@gmail.com>
2020-08-06 16:31:35 -0700

    feat(translation-core): added AxiomInProjectTranslator in the translator module

--- owl2lpg-translation-core/src/main/java/edu/stanford/owl2lpg/translator/TranslatorComponent.java
+++ owl2lpg-translation-core/src/main/java/edu/stanford/owl2lpg/translator/TranslatorComponent.java
@@ -17,6 +17,8 @@ public interface TranslatorComponent {
 
   AxiomTranslator getAxiomTranslator();
 
+  AxiomInProjectTranslator getAxiomInProjectTranslator();
+
   OntologyDocumentAxiomTranslator getOntologyDocumentAxiomTranslator();
 
   OntologyInProjectTranslator getOntologyInProjectTranslator();
