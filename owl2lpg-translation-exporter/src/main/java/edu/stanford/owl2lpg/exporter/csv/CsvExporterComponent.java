aaa2e90364d75939b59929816af6aad0e7a98c94
Josef Hardi <josef.hardi@gmail.com>
2020-08-07 10:17:38 -0700

    feat(translation-exporter): added 2 different CSV exporters in the component

--- owl2lpg-translation-exporter/src/main/java/edu/stanford/owl2lpg/exporter/csv/CsvExporterComponent.java
+++ owl2lpg-translation-exporter/src/main/java/edu/stanford/owl2lpg/exporter/csv/CsvExporterComponent.java
@@ -1,15 +1,18 @@
 package edu.stanford.owl2lpg.exporter.csv;
 
 import dagger.Component;
+import edu.stanford.owl2lpg.exporter.ProjectContextModule;
 import edu.stanford.owl2lpg.translator.TranslationSessionScope;
 import edu.stanford.owl2lpg.translator.TranslatorModule;
 
 @Component(modules = {
-    CsvExporterModule.class, TranslatorModule.class
-})
+    ProjectContextModule.class,
+    TranslatorModule.class,
+    CsvWriterModule.class})
 @TranslationSessionScope
 public interface CsvExporterComponent {
 
-    CsvExporterFactory getCsvExporterFactory();
+  OntologyCsvExporter getOntologyCsvExporter();
 
+  PerAxiomCsvExporter getPerAxiomCsvExporter();
 }
---
+++ owl2lpg-translation-exporter/src/main/java/edu/stanford/owl2lpg/exporter/csv/PerAxiomCsvExporter.java
@@ -0,0 +1,51 @@
+package edu.stanford.owl2lpg.exporter.csv;
+
+import edu.stanford.owl2lpg.translator.AxiomInProjectTranslator;
+import org.semanticweb.owlapi.model.OWLAxiom;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * @author Josef Hardi <josef.hardi@stanford.edu> <br>
+ * Stanford Center for Biomedical Informatics Research
+ */
+public class PerAxiomCsvExporter {
+
+  @Nonnull
+  private final AxiomInProjectTranslator axiomTranslator;
+
+  @Nonnull
+  private final Neo4jCsvWriter csvWriter;
+
+  @Inject
+  public PerAxiomCsvExporter(@Nonnull AxiomInProjectTranslator axiomTranslator,
+                             @Nonnull Neo4jCsvWriter csvWriter) {
+    this.axiomTranslator = checkNotNull(axiomTranslator);
+    this.csvWriter = checkNotNull(csvWriter);
+  }
+
+  public void export(@Nonnull OWLAxiom axiom) throws IOException {
+    var translation = axiomTranslator.translate(axiom);
+    csvWriter.writeTranslation(translation);
+    csvWriter.flush();
+
+    var console = new PrintWriter(System.out);
+
+    console.printf("\nNodes: %,d\n\n", csvWriter.getNodeCount());
+    csvWriter.getNodeLabelsMultiset()
+        .forEachEntry((nodeLabels, count) ->
+            console.printf("    Node   %-60s %,10d\n", nodeLabels.getNeo4jName(), count));
+
+    console.printf("\nRelationships: %,d\n\n", csvWriter.getEdgeCount());
+    csvWriter.getEdgeLabelMultiset()
+        .forEachEntry((edgeLabel, count) ->
+            console.printf("    Rel    %-36s %,10d\n", edgeLabel.getNeo4jName(), count));
+
+    console.flush();
+  }
+}
