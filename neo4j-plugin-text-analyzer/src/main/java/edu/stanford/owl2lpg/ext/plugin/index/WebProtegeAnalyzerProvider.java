4f2fbff46fa0875f7e065f3ecec19947788776a3
Josef Hardi <josef.hardi@gmail.com>
2020-08-13 13:30:41 -0700

    fix(neo4j-plugin): tweak the analyzer configuration

--- neo4j-plugin-text-analyzer/src/main/java/edu/stanford/owl2lpg/ext/plugin/index/WebProtegeAnalyzerProvider.java
+++ neo4j-plugin-text-analyzer/src/main/java/edu/stanford/owl2lpg/ext/plugin/index/WebProtegeAnalyzerProvider.java
@@ -2,10 +2,11 @@ package edu.stanford.owl2lpg.ext.plugin.index;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.core.WhitespaceTokenizerFactory;
 import org.apache.lucene.analysis.custom.CustomAnalyzer;
 import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory;
+import org.apache.lucene.analysis.miscellaneous.WordDelimiterGraphFilterFactory;
 import org.apache.lucene.analysis.ngram.EdgeNGramFilterFactory;
-import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
 import org.neo4j.graphdb.schema.AnalyzerProvider;
 
 import java.io.IOException;
@@ -23,20 +24,22 @@ public class WebProtegeAnalyzerProvider extends AnalyzerProvider {
 
   @Override
   public String description() {
-    return "Analyzer that uses a standard tokenizer with ASCII folding, lowercase, and edge n-gram filters";
+    return "Analyzer that uses the whitespace tokenizer with ASCII folding, edge n-gram filter, word delimiter graph and lowercase.";
   }
 
   @Override
   public Analyzer createAnalyzer() {
     try {
       return CustomAnalyzer.builder()
-          .withTokenizer(StandardTokenizerFactory.NAME)
+          .withTokenizer(WhitespaceTokenizerFactory.NAME)
+          .addTokenFilter(WordDelimiterGraphFilterFactory.NAME,
+              "preserveOriginal", "1")
+          .addTokenFilter(EdgeNGramFilterFactory.NAME,
+              "minGramSize", "3",
+              "maxGramSize", "8",
+              "preserveOriginal", "true")
           .addTokenFilter(ASCIIFoldingFilterFactory.NAME)
           .addTokenFilter(LowerCaseFilterFactory.NAME)
-          .addTokenFilter(EdgeNGramFilterFactory.NAME,
-              "minGramSize", "2",
-              "maxGramSize", "10",
-              "preserveOriginal", "true")
           .build();
     } catch (IOException e) {
       throw new UncheckedIOException(e);
