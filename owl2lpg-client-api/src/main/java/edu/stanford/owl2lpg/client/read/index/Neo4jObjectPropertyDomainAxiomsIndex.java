edcb4b4a069825bf601865c73ada1ab482bf8f29
Josef Hardi <josef.hardi@gmail.com>
2020-08-20 12:32:36 -0700

    feat(client-api): implemented OP domain and range indexes

---
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/index/Neo4jObjectPropertyDomainAxiomsIndex.java
@@ -0,0 +1,40 @@
+package edu.stanford.owl2lpg.client.read.index;
+
+import edu.stanford.bmir.protege.web.server.index.ObjectPropertyDomainAxiomsIndex;
+import edu.stanford.owl2lpg.client.read.axiom.AxiomContext;
+import edu.stanford.owl2lpg.client.read.axiom.DomainAxiomAccessor;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLObjectPropertyDomainAxiom;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.stream.Stream;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * @author Josef Hardi <josef.hardi@stanford.edu> <br>
+ * Stanford Center for Biomedical Informatics Research
+ */
+public class Neo4jObjectPropertyDomainAxiomsIndex implements ObjectPropertyDomainAxiomsIndex {
+
+  @Nonnull
+  private final AxiomContext axiomContext;
+
+  @Nonnull
+  private final DomainAxiomAccessor domainAxiomAccessor;
+
+  @Inject
+  public Neo4jObjectPropertyDomainAxiomsIndex(@Nonnull AxiomContext axiomContext,
+                                              @Nonnull DomainAxiomAccessor domainAxiomAccessor) {
+    this.axiomContext = checkNotNull(axiomContext);
+    this.domainAxiomAccessor = checkNotNull(domainAxiomAccessor);
+  }
+
+  @Nonnull
+  @Override
+  public Stream<OWLObjectPropertyDomainAxiom> getObjectPropertyDomainAxioms(@Nonnull OWLObjectProperty owlObjectProperty, @Nonnull OWLOntologyID owlOntologyID) {
+    return domainAxiomAccessor.getObjectPropertyDomainAxioms(owlObjectProperty, axiomContext).stream();
+  }
+}
---
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/read/index/Neo4jObjectPropertyRangeAxiomsIndex.java
@@ -0,0 +1,40 @@
+package edu.stanford.owl2lpg.client.read.index;
+
+import edu.stanford.bmir.protege.web.server.index.ObjectPropertyRangeAxiomsIndex;
+import edu.stanford.owl2lpg.client.read.axiom.AxiomContext;
+import edu.stanford.owl2lpg.client.read.axiom.RangeAxiomAccessor;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLObjectPropertyRangeAxiom;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.stream.Stream;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * @author Josef Hardi <josef.hardi@stanford.edu> <br>
+ * Stanford Center for Biomedical Informatics Research
+ */
+public class Neo4jObjectPropertyRangeAxiomsIndex implements ObjectPropertyRangeAxiomsIndex {
+
+  @Nonnull
+  private final AxiomContext axiomContext;
+
+  @Nonnull
+  private final RangeAxiomAccessor rangeAxiomAccessor;
+
+  @Inject
+  public Neo4jObjectPropertyRangeAxiomsIndex(@Nonnull AxiomContext axiomContext,
+                                             @Nonnull RangeAxiomAccessor rangeAxiomAccessor) {
+    this.axiomContext = checkNotNull(axiomContext);
+    this.rangeAxiomAccessor = checkNotNull(rangeAxiomAccessor);
+  }
+
+  @Nonnull
+  @Override
+  public Stream<OWLObjectPropertyRangeAxiom> getObjectPropertyRangeAxioms(@Nonnull OWLObjectProperty owlObjectProperty, @Nonnull OWLOntologyID owlOntologyID) {
+    return rangeAxiomAccessor.getObjectPropertyRangeAxioms(owlObjectProperty, axiomContext).stream();
+  }
+}
