5ddf1c1f0dccc355c7d72a458be509dbb17b601a
Josef Hardi <josef.hardi@gmail.com>
2020-08-31 17:57:17 -0700

    feat(translation-exporter): export OBO id field to CSV column

--- owl2lpg-translation-exporter/src/main/java/edu/stanford/owl2lpg/exporter/csv/Neo4jNodeCsvSchema.java
+++ owl2lpg-translation-exporter/src/main/java/edu/stanford/owl2lpg/exporter/csv/Neo4jNodeCsvSchema.java
@@ -11,35 +11,36 @@ import static edu.stanford.owl2lpg.translator.vocab.PropertyFields.*;
 
 public class Neo4jNodeCsvSchema implements Neo4jCsvSchema {
 
-    @Inject
-    public Neo4jNodeCsvSchema() {
-    }
+  @Inject
+  public Neo4jNodeCsvSchema() {
+  }
 
-    @Override
-    @Nonnull
-    public CsvSchema getCsvSchema() {
-        return getBuilder().build();
-    }
+  @Override
+  @Nonnull
+  public CsvSchema getCsvSchema() {
+    return getBuilder().build();
+  }
 
-    @Override
-    @Nonnull
-    public CsvSchema getCsvSchemaWithHeader() {
-        return getBuilder().setUseHeader(true).build();
-    }
+  @Override
+  @Nonnull
+  public CsvSchema getCsvSchemaWithHeader() {
+    return getBuilder().setUseHeader(true).build();
+  }
 
-    private static CsvSchema.Builder getBuilder() {
-        return CsvSchema.builder()
-                        .addColumn(N4J_JSON_ID)
-                        .addColumn(N4J_JSON_LABELS)
-                        .addColumn(PROJECT_ID)
-                        .addColumn(BRANCH_ID)
-                        .addColumn(ONTOLOGY_DOCUMENT_ID)
-                        .addColumn(IRI)
-                        .addColumn(IRI_SUFFIX)
-                        .addColumn(LEXICAL_FORM)
-                        .addColumn(DATATYPE)
-                        .addColumn(LANGUAGE)
-                        .addColumn(NODE_ID)
-                        .addColumn(CARDINALITY + ":int");
-    }
+  private static CsvSchema.Builder getBuilder() {
+    return CsvSchema.builder()
+        .addColumn(N4J_JSON_ID)
+        .addColumn(N4J_JSON_LABELS)
+        .addColumn(PROJECT_ID)
+        .addColumn(BRANCH_ID)
+        .addColumn(ONTOLOGY_DOCUMENT_ID)
+        .addColumn(IRI)
+        .addColumn(IRI_SUFFIX)
+        .addColumn(OBO_ID)
+        .addColumn(LEXICAL_FORM)
+        .addColumn(DATATYPE)
+        .addColumn(LANGUAGE)
+        .addColumn(NODE_ID)
+        .addColumn(CARDINALITY + ":int");
+  }
 }
