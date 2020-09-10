7f47a0932237250598d62760963d476ce1e54c72
Josef Hardi <josef.hardi@gmail.com>
2020-09-10 11:33:56 -0700

    feat(client-api): added Neo4j implementation of ProjectSignatureIndex

---
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/bind/index/Neo4jProjectSignatureIndex.java
@@ -0,0 +1,42 @@
+package edu.stanford.owl2lpg.client.bind.index;
+
+import edu.stanford.bmir.protege.web.server.index.ProjectSignatureIndex;
+import edu.stanford.owl2lpg.client.read.ontology.ProjectAccessor;
+import edu.stanford.owl2lpg.model.BranchId;
+import edu.stanford.owl2lpg.model.ProjectId;
+import org.semanticweb.owlapi.model.OWLEntity;
+
+import javax.annotation.Nonnull;
+import java.util.stream.Stream;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * @author Josef Hardi <josef.hardi@stanford.edu> <br>
+ * Stanford Center for Biomedical Informatics Research
+ */
+public class Neo4jProjectSignatureIndex implements ProjectSignatureIndex {
+
+  @Nonnull
+  private final ProjectId projectId;
+
+  @Nonnull
+  private final BranchId branchId;
+
+  @Nonnull
+  private final ProjectAccessor projectAccessor;
+
+  public Neo4jProjectSignatureIndex(@Nonnull ProjectId projectId,
+                                    @Nonnull BranchId branchId,
+                                    @Nonnull ProjectAccessor projectAccessor) {
+    this.projectId = checkNotNull(projectId);
+    this.branchId = checkNotNull(branchId);
+    this.projectAccessor = checkNotNull(projectAccessor);
+  }
+
+  @Nonnull
+  @Override
+  public Stream<OWLEntity> getSignature() {
+    return projectAccessor.getAllEntities(projectId, branchId);
+  }
+}
