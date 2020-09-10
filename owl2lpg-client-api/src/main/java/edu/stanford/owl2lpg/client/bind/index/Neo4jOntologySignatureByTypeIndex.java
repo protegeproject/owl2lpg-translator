f53f74e10eb93bf8cd42e5dd6531df57fe93d059
Josef Hardi <josef.hardi@gmail.com>
2020-09-10 10:48:29 -0700

    feat(client-api): added Neo4j implementation of OntologySIgnatureByTypeIndex

---
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/bind/index/Neo4jOntologySignatureByTypeIndex.java
@@ -0,0 +1,51 @@
+package edu.stanford.owl2lpg.client.bind.index;
+
+import edu.stanford.bmir.protege.web.server.index.OntologySignatureByTypeIndex;
+import edu.stanford.owl2lpg.client.read.ontology.OntologyAccessor;
+import edu.stanford.owl2lpg.model.BranchId;
+import edu.stanford.owl2lpg.model.OntologyDocumentId;
+import edu.stanford.owl2lpg.model.ProjectId;
+import org.semanticweb.owlapi.model.EntityType;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLOntologyID;
+
+import javax.annotation.Nonnull;
+import java.util.stream.Stream;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * @author Josef Hardi <josef.hardi@stanford.edu> <br>
+ * Stanford Center for Biomedical Informatics Research
+ */
+public class Neo4jOntologySignatureByTypeIndex implements OntologySignatureByTypeIndex {
+
+  @Nonnull
+  private final ProjectId projectId;
+
+  @Nonnull
+  private final BranchId branchId;
+
+  @Nonnull
+  private final OntologyDocumentId ontoDocId;
+
+  @Nonnull
+  private final OntologyAccessor ontologyAccessor;
+
+  public Neo4jOntologySignatureByTypeIndex(@Nonnull ProjectId projectId,
+                                           @Nonnull BranchId branchId,
+                                           @Nonnull OntologyDocumentId ontoDocId,
+                                           @Nonnull OntologyAccessor ontologyAccessor) {
+    this.projectId = checkNotNull(projectId);
+    this.branchId = checkNotNull(branchId);
+    this.ontoDocId = checkNotNull(ontoDocId);
+    this.ontologyAccessor = checkNotNull(ontologyAccessor);
+  }
+
+  @Nonnull
+  @Override
+  public <E extends OWLEntity> Stream<E> getSignature(@Nonnull EntityType<E> entityType,
+                                                      @Nonnull OWLOntologyID owlOntologyID) {
+    return ontologyAccessor.getEntitiesByType(entityType, projectId, branchId, ontoDocId).stream();
+  }
+}
