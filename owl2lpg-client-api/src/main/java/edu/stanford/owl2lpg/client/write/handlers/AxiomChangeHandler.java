def6849cd05e83631b4a020ff025771a934abc89
Josef Hardi <josef.hardi@gmail.com>
2020-09-29 16:43:19 -0700

    fix: removed project context form the interface methods

--- owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/write/handlers/AxiomChangeHandler.java
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/write/handlers/AxiomChangeHandler.java
@@ -1,8 +1,5 @@
 package edu.stanford.owl2lpg.client.write.handlers;
 
-import edu.stanford.owl2lpg.model.BranchId;
-import edu.stanford.owl2lpg.model.OntologyDocumentId;
-import edu.stanford.owl2lpg.model.ProjectId;
 import org.semanticweb.owlapi.model.OWLAxiom;
 
 import javax.annotation.Nonnull;
@@ -11,15 +8,9 @@ import javax.annotation.Nonnull;
  * @author Josef Hardi <josef.hardi@stanford.edu> <br>
  * Stanford Center for Biomedical Informatics Research
  */
-public interface AxiomChangeHandler<T extends OWLAxiom> {
+public interface AxiomChangeHandler {
 
-  void handleAdd(@Nonnull ProjectId projectId,
-                 @Nonnull BranchId branchId,
-                 @Nonnull OntologyDocumentId ontoDocId,
-                 @Nonnull T axiom);
+  void handleAdd(@Nonnull OWLAxiom axiom);
 
-  void handleRemove(@Nonnull ProjectId projectId,
-                    @Nonnull BranchId branchId,
-                    @Nonnull OntologyDocumentId ontoDocId,
-                    @Nonnull T axiom);
+  void handleRemove(@Nonnull OWLAxiom axiom);
 }
