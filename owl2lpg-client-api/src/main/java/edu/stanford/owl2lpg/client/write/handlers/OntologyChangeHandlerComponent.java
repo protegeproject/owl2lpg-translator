ce104f71049098a34152e276c781cbf603c8621a
Josef Hardi <josef.hardi@gmail.com>
2020-10-23 09:45:50 -0700

    feat: created a component to get ontology change handlers

---
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/write/handlers/OntologyChangeHandlerComponent.java
@@ -0,0 +1,25 @@
+package edu.stanford.owl2lpg.client.write.handlers;
+
+import dagger.Component;
+import edu.stanford.bmir.protege.web.shared.inject.ProjectSingleton;
+import edu.stanford.owl2lpg.client.DatabaseModule;
+import edu.stanford.owl2lpg.translator.OntologyContextModule;
+import edu.stanford.owl2lpg.translator.shared.OntologyObjectSerializerModule;
+
+/**
+ * @author Josef Hardi <josef.hardi@stanford.edu> <br>
+ * Stanford Center for Biomedical Informatics Research
+ */
+@Component(modules = {
+    DatabaseModule.class,
+    OntologyContextModule.class,
+    OntologyObjectSerializerModule.class,
+    OntologyChangeHandlerModule.class
+})
+@ProjectSingleton
+public interface OntologyChangeHandlerComponent {
+
+  AxiomChangeHandler getAxiomChangeHandler();
+
+  OntologyAnnotationChangeHandler getOntologyAnnotationChangeHandler();
+}
