46e66c5907da7cc8d16fcd6352af1f43150bd767
Josef Hardi <josef.hardi@gmail.com>
2020-10-25 16:24:47 -0700

    fix: removed unused module

--- owl2lpg-translation-exporter/src/main/java/edu/stanford/owl2lpg/exporter/csv/CsvExporterComponent.java
+++ owl2lpg-translation-exporter/src/main/java/edu/stanford/owl2lpg/exporter/csv/CsvExporterComponent.java
@@ -1,14 +1,12 @@
 package edu.stanford.owl2lpg.exporter.csv;
 
 import dagger.Component;
-import edu.stanford.owl2lpg.exporter.ProjectContextModule;
 import edu.stanford.owl2lpg.exporter.csv.writer.CsvWriterModule;
 import edu.stanford.owl2lpg.translator.TranslationSessionScope;
 import edu.stanford.owl2lpg.translator.TranslatorModule;
 import edu.stanford.owl2lpg.translator.shared.OntologyObjectSerializerModule;
 
 @Component(modules = {
-    ProjectContextModule.class,
     TranslatorModule.class,
     CsvWriterModule.class,
     OntologyObjectSerializerModule.class})
