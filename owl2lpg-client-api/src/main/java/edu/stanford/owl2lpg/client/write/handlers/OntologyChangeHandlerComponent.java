30286514eb97c75d4c15eb3c276e95e2702a7375
Josef Hardi <josef.hardi@gmail.com>
2020-10-25 18:46:01 -0700

    fix(owl2lpg-client-api): switch to the ontology project module

--- owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/write/handlers/OntologyChangeHandlerComponent.java
+++ owl2lpg-client-api/src/main/java/edu/stanford/owl2lpg/client/write/handlers/OntologyChangeHandlerComponent.java
@@ -3,7 +3,7 @@ package edu.stanford.owl2lpg.client.write.handlers;
 import dagger.Component;
 import edu.stanford.bmir.protege.web.shared.inject.ProjectSingleton;
 import edu.stanford.owl2lpg.client.DatabaseModule;
-import edu.stanford.owl2lpg.translator.OntologyContextModule;
+import edu.stanford.owl2lpg.client.OntologyProjectModule;
 import edu.stanford.owl2lpg.translator.shared.OntologyObjectSerializerModule;
 
 /**
@@ -12,9 +12,9 @@ import edu.stanford.owl2lpg.translator.shared.OntologyObjectSerializerModule;
  */
 @Component(modules = {
     DatabaseModule.class,
-    OntologyContextModule.class,
-    OntologyObjectSerializerModule.class,
-    OntologyChangeHandlerModule.class
+    OntologyProjectModule.class,
+    OntologyChangeHandlerModule.class,
+    OntologyObjectSerializerModule.class
 })
 @ProjectSingleton
 public interface OntologyChangeHandlerComponent {
