2817941d537f9cc20e524c2b5d6b67be3935830d
fred eisele <fred.eisele@gmail.com>
2020-12-17 18:05:21 -0600

    update script

--- owl2lpg-translation-cli/src/main/scripts/run.ps1
+++ owl2lpg-translation-cli/src/main/scripts/run.ps1
@@ -1,19 +1,8 @@
 
-#
-#$params = @{
-#  FilePath = [string]::Format("{0}\bin\java.exe",$env:JAVA_HOME)
-##  WorkingDirectory = "D:\translation\config\"
-#  ArgumentList = @("-Xms8g","-Xmx16g", "-jar", "owl2lpg-translation-cli.jar", "translate","--help")
-#  RedirectStandardError = "c:\temp\JavaErrorInTranslation.txt"
-#  PassThru = $true
-#  Wait = $true
-#}
 $rc = Start-Process `
     -FilePath "C:\Program Files\OpenJDK\jdk-14.0.1\bin\java.exe" `
     -ArgumentList "-Xms8g","-Xmx16g","-jar",".\owl2lpg-translation-cli-1.0-SNAPSHOT-shaded.jar","translate","-help" `
     -NoNewWindow
-
-#$rc = Start-Process $params
 
 if ($rc.ExitCode -eq 0)
 {
